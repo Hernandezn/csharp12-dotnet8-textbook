8ada3e39d0166d01867bd15b763493ac745180f1
hernandezn <hernandezn@live.com>
2026-03-13 18:40:35 -0400

    Chapter 1 - Added VSCode-initialized Files

---
+++ Ch01_hello-csharp-welcome-.net/vscode/Chapter01/HelloCS/Program.cs
@@ -0,0 +1,6 @@
+﻿
+Console.WriteLine("C# again, from the top!");
+
+string name = typeof(Program).Namespace ?? "None!"; // nullish coalescing operator
+Console.WriteLine($"Namespace: {name}"); // string template
+throw new Exception(); // to see the stack trace that includes the implicitly declared Program class
---
+++ Ch01_hello-csharp-welcome-.net/vscode/Chapter01/MyEnvironment/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+    this project created using console command:
+
+        dotnet new console -o MyEnvironment --use-program-main
+*/
+
+namespace MyEnvironment;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // string templating
+        Console.WriteLine(
+            $"Directory: {Environment.CurrentDirectory}\nOS Version: {Environment.OSVersion.VersionString}"
+        );
+
+        // a string arg insertion / format string syntax
+        Console.WriteLine(
+            "Namespace: {0}",
+            typeof(Program).Namespace ?? "None!"
+        );
+    }
+}
