6811613ff69a342b59f1144031b6fb5b23a60be4
hernandezn <hernandezn@live.com>
2026-03-13 17:26:24 -0400

    Chapter 1 Initialization with Gitignore Fix for Visual Studio

---
+++ Ch01_hello-csharp-welcome-.net/visual-studio/Chapter01/HelloCS/Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.WriteLine("Back to C#!");
+
+string name = typeof(Program).Namespace ?? "None!"; // nullish coalescing
+Console.WriteLine($"Namespace: {name}");
+
+throw new Exception(); // reveals this program's implicit class in its stack trace
---
+++ Ch01_hello-csharp-welcome-.net/visual-studio/Chapter01/MyEnvironment/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MyEnvironment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // a string templating syntax
+            Console.WriteLine(
+                $"Directory: {Environment.CurrentDirectory}\nOS Version: {Environment.OSVersion.VersionString}"
+            );
+
+            // a string arg insertion / format string syntax
+            Console.WriteLine(
+                "Namespace: {0}",
+                typeof(Program).Namespace ?? "None!"
+            );
+        }
+    }
+}
