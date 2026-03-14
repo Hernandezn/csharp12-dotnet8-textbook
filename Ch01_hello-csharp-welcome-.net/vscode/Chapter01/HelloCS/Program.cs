aa013df88ad3eb55452701d344091bde048fe39f
hernandezn <hernandezn@live.com>
2026-03-14 19:52:54 -0400

    Chapter 1 - Added Console Args Demonstration & Interoperability Note

--- Ch01_hello-csharp-welcome-.net/vscode/Chapter01/HelloCS/Program.cs
+++ Ch01_hello-csharp-welcome-.net/vscode/Chapter01/HelloCS/Program.cs
@@ -1,6 +1,11 @@
 ﻿
 Console.WriteLine("C# again, from the top!");
 
+string argStatement = args[0] ?? "No args";
+Console.WriteLine($"First program arg: {argStatement}");
+
 string name = typeof(Program).Namespace ?? "None!"; // nullish coalescing operator
 Console.WriteLine($"Namespace: {name}"); // string template
 throw new Exception(); // to see the stack trace that includes the implicitly declared Program class
+
+// non-executing code samples below
