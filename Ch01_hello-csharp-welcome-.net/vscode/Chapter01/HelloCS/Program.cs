7dcd89372f2bf7172d7d3a7b6cee53ce5bd254b6
hernandezn <hernandezn@live.com>
2026-03-14 20:01:16 -0400

    Removed Non-functioning Lines from Chapter 1 Demo Code

--- Ch01_hello-csharp-welcome-.net/vscode/Chapter01/HelloCS/Program.cs
+++ Ch01_hello-csharp-welcome-.net/vscode/Chapter01/HelloCS/Program.cs
@@ -7,5 +7,3 @@ Console.WriteLine($"First program arg: {argStatement}");
 string name = typeof(Program).Namespace ?? "None!"; // nullish coalescing operator
 Console.WriteLine($"Namespace: {name}"); // string template
 throw new Exception(); // to see the stack trace that includes the implicitly declared Program class
-
-// non-executing code samples below
